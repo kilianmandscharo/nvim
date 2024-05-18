840adac9a0a7cc96c834f20a05f2ee207a7fdd8e
Dominik Heller <kilianmandscharo@gmail.com>
2024-05-18 11:54:59 +0200

    Add keymap to toggle quickfix list

---
+++ plugin/keymaps.lua
@@ -0,0 +1,36 @@
+local set = vim.keymap.set;
+
+-- Formatting
+set("n", "<leader>f", ":lua vim.lsp.buf.format()<CR>")
+
+-- Window navigation
+set("n", "<C-h>", "<C-w>h")
+set("n", "<C-j>", "<C-w>j")
+set("n", "<C-k>", "<C-w>k")
+set("n", "<C-l>", "<C-w>l")
+
+-- Buffer navigation
+set("n", "<S-l>", ":bnext<CR>")
+set("n", "<S-h>", ":bprevious<CR>")
+set("n", "<leader>c", ":bdelete<CR>")
+
+-- Terminal
+set("t", "<Esc>", "<C-\\><C-n>")
+
+-- Toggle Quick Fix List
+set("n", "<leader>b", function()
+  local qf_open = false
+
+  for _, win in pairs(vim.fn.getwininfo()) do
+    if win["quickfix"] == 1 then
+      qf_open = true
+      break
+    end
+  end
+
+  if qf_open == true then
+    vim.cmd("cclose")
+  else
+    vim.cmd("copen")
+  end
+end)
---
+++ plugin/options.lua
@@ -0,0 +1,24 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.incsearch = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.signcolumn = "yes"
+
+vim.g.netrw_banner = 0
+vim.g.netrw_keepdir = 0
+vim.g.netrw_winsize = 20
+
+vim.opt.colorcolumn = "80"
+vim.opt.laststatus = 3
+vim.opt.winbar = "%=%m %f"
+
+vim.cmd.colorscheme("catppuccin-mocha")
